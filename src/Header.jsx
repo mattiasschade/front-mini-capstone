2c502ac3f6d375858411d816bdc0c7bdeae0ec0c
Mattias Schade <mattiasschade@gmail.com>
2023-05-25 12:31:04 -0500

    add auth links conditionally

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,10 +2,18 @@ import { Link } from "react-router-dom";
 import { LogoutLink } from "./Logout";
 
 export function Header () {
+  let authenticationLinks;
+  if (localStorage.jwt === undefined) {
+    authenticationLinks = <>
+      <a href="/login">Login</a> | <a href="/signup">Signup</a>
+      </>
+  } else {
+    authenticationLinks = <LogoutLink />
+  }
   return (
     <header>
       <nav>
-        <a href="/">Home</a> | <Link to="/products/new">New Product</Link> | <Link to="/about">About</Link>
+        <a href="/">Home</a> | <Link to="/products/new">New Product</Link> | <Link to="/about">About</Link> | {authenticationLinks}
       </nav>
     </header>
   )
