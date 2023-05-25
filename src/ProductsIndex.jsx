30517c522dc3ea0cf4b9bb13ba8f008557cee8b5
Mattias Schade <mattiasschade@gmail.com>
2023-05-25 13:00:38 -0500

    Products Search

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
+
 export function ProductsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+  
   return (
     <div>
     <h1> Products </h1>
-    {props.products.map((product) => (
+    <p> Search <input type="text" value={searchFilter} onChange={(event) => {setSearchFilter(event.target.value)}} /> </p>
+      {props.products.filter(product => product.name.toLowerCase().includes(searchFilter.toLowerCase())).map((product) => (
       <div key={product.id}>
         <h2>{product.name}</h2>
         <h3>${product.price}</h3>
